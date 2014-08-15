9f5575189978143694fe96f47e6dbe1ab1fef962
ahaak <alex.haak@erasmusmc.nl>
2014-08-15 19:53:10 +0200

    finished multithreading of video capture

--- VideoBuffer.cpp
+++ VideoBuffer.cpp
@@ -1,5 +1,9 @@
 #include <iostream>            // c++ I/O
-#include "opencv2/highgui/highgui.hpp"
+
+#include "opencv2/opencv.hpp"
+
+#include <windows.h>          // for HANDLE
+#include <process.h>          // for _beginthread()
 
 #include <boost/thread/condition.hpp>
 #include <boost/thread/mutex.hpp>
@@ -45,16 +49,171 @@ private:
   boost::circular_buffer<T> cb;
 };
 
+class captureVideo        // this is the producer
+{
+  circ_buffer<cv::Mat>* m_ringBuffer;
+  cv::VideoCapture m_cap;
+public:
+  captureVideo( circ_buffer<cv::Mat>* buff )
+  {
+    m_ringBuffer = buff;
+  }
+
+  void acquirerVideoEntryPoint()
+  {
+    m_cap.open(0);
+    if ( !m_cap.isOpened() )
+    {
+      std::cout << "Video capture could not be opened. ABORT!" << std::endl;
+    }
+    else
+    {
+      cv::Mat frame;
+      int itr = 0;
+
+      for (;;)
+      {
+        m_cap >> frame;
+        cv::cvtColor( frame, frame, cv::COLOR_BGR2GRAY );
+        double minVal, maxVal;
+        int minInd, maxInd;
+        cv::minMaxIdx( frame, &minVal, &maxVal, &minInd, &maxInd, cv::Mat() );
+        cv::Scalar meanVal = cv::mean( frame, cv::Mat() );
+        m_ringBuffer->send( frame );
+        std::cout << "SET frame at iteration " << itr << ". Buffer size: " << m_ringBuffer->size() << ". Mean intensity in frame: " << meanVal[0] << std::endl;
+        ++itr;
+      }
+    }   
+  }
+
+  static unsigned __stdcall acquirerVideoStaticEntryPoint(void * pThis)
+  {
+    captureVideo* pCV = (captureVideo*)pThis;
+    pCV->acquirerVideoEntryPoint();
+    return 2;
+  }
+
+};
+
+class showVideo
+{
+  circ_buffer<cv::Mat>* m_ringBuffer;
+  
+public:
+  showVideo( circ_buffer<cv::Mat>* buff )
+  {
+    m_ringBuffer = buff;
+    
+  }
+
+  void showVideoEntryPoint()
+  {
+    cv::Mat frame;
+    int itr = 0;
+    //cv::namedWindow( "threadedVideoShow",  cv::WINDOW_AUTOSIZE );
+    for (;;)
+    {
+      if ( m_ringBuffer->size() > 2 )
+	    {
+	      frame = m_ringBuffer->receive();
+	      cv::imshow( "threadedVideoShow", frame );
+        cv::waitKey(10);
+        cv::Scalar meanVal = cv::mean( frame, cv::Mat() );
+        std::cout << "GET frame at iteration " << itr << ". Buffer size: " << m_ringBuffer->size() << ". Mean intensity in frame: " << meanVal[0] << std::endl;
+        ++itr;
+	    }
+    }
+    
+  }
+
+   static unsigned __stdcall showVideoStaticEntryPoint( void* pThis )
+   {
+     showVideo* pSV = (showVideo*)pThis;
+     pSV->showVideoEntryPoint();
+     return 2;
+   }
+
+};
+
 int main()
 {
-  cv::Mat test;
+  DWORD   dwExitCode;
+  // circular buffer
+  circ_buffer<cv::Mat> *ringBuffer = new circ_buffer<cv::Mat>(10);
+  std::cout << "Initialized ring buffer. Buffer size: " << ringBuffer->size() << std::endl;
 
-  circ_buffer<cv::Mat> * test2 = new circ_buffer<cv::Mat>(5);
-  std::cout << "Size Buffer: " << test2->size();
-  test2->send( test );
-  std::cout << "Size Buffer: " << test2->size();
-  cv::Mat test3 = test2->receive();
-  std::cout << "Size Buffer: " << test2->size();
+  // threaded video show
+  showVideo* showThreaded = new showVideo( ringBuffer );
+
+  HANDLE hthshowThreaded;
+  unsigned uishowThreadedThreadID;
+
+  hthshowThreaded = (HANDLE)_beginthreadex( NULL, 0, showVideo::showVideoStaticEntryPoint, showThreaded, CREATE_SUSPENDED, &uishowThreadedThreadID );
+  if ( hthshowThreaded == 0 )
+  {
+    std::cout << "Failed to created threaded video show." << std::endl;
+  }
+  GetExitCodeThread( hthshowThreaded, &dwExitCode );
+  std::cout << "initial Show video thread exit code = " << dwExitCode << std::endl;
+
+  ResumeThread( hthshowThreaded );
+  WaitForSingleObject(hthshowThreaded, 10 );
+  
+  // threaded video capture
+  captureVideo* capThreaded = new captureVideo( ringBuffer );
+  
+  HANDLE  hthcapThreaded;
+  unsigned  uicapThreadedThreadID;
+
+  hthcapThreaded = (HANDLE)_beginthreadex( NULL, 0, captureVideo::acquirerVideoStaticEntryPoint, capThreaded, CREATE_SUSPENDED,  &uicapThreadedThreadID );
+  
+  if ( hthcapThreaded == 0 )
+  {
+    std::cout << "Failed to created threaded video capture." << std::endl;
+  }
+   GetExitCodeThread( hthcapThreaded, &dwExitCode );
+   std::cout << "initial capture Video thread exit code = " << dwExitCode << std::endl;
+
+   ResumeThread( hthcapThreaded );
+   WaitForSingleObject( hthshowThreaded, 1000000 );
+
+  // Video capture
+  cv::VideoCapture cap(0);
+  if ( !cap.isOpened() )
+  {
+    return -1;
+  }
+
+  cv::Mat frame;
+  cv::Mat blur;
+  cv::Mat edges;
+  
+  cv::namedWindow("frame",1);
+  cv::namedWindow("blur",2);
+  cv::namedWindow("edges",3);
+
+  int itr = 0;
+  for (;;)
+  {
+    cap >> frame;
+    
+    ringBuffer->send( frame );
+    std::cout << "Set frame at iteration " << itr << ". Buffer size: " << ringBuffer->size() << std::endl;
+    ++itr;
+
+    cv::cvtColor( frame, blur, cv::COLOR_BGR2GRAY);
+    cv::GaussianBlur( blur, blur, cv::Size(7,7), 3.5, 3.5);
+    cv::Canny(blur, edges, 0, 30, 3);
+    
+    cv::imshow("frame", frame);
+    cv::imshow("blur", blur);
+    cv::imshow("edges", edges);
+    if(cv::waitKey(30) >= 0) 
+    {
+      break;
+    }
+  }
+
   
 
   std::cin.ignore();
